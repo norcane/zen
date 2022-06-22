f473140a2895041aa8827072e4b54a46d8e09f63
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2022-06-22 21:57:04 +0200

    Fix run of native image, fix incorrect names

--- src/main/java-templates/com/norcane/zen/meta/BuildInfo.java
+++ src/main/java-templates/com/norcane/zen/meta/BuildInfo.java
@@ -4,5 +4,5 @@ public class BuildInfo {
 
     public static final String NAME = "${project.name}";
     public static final String DESCRIPTION = "${project.description}";
-    public static final String VERSION = "${project.minCompatibleVersion}";
+    public static final String VERSION = "${project.version}";
 }
--- src/main/java/com/norcane/zen/config/yaml/model/YamlAppConfig.java
+++ src/main/java/com/norcane/zen/config/yaml/model/YamlAppConfig.java
@@ -3,6 +3,9 @@ package com.norcane.zen.config.yaml.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.norcane.zen.meta.SemVer;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record YamlAppConfig(
     @JsonProperty("min-compatible-version") SemVer minCompatibleVersion
 ) {
